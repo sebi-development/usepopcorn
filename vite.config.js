ff6a18fb310fbdb144f6238cec80767a07649416
Sebik <paj0029@vsb.cz>
2026-09-21 16:08:46 +0200

    chore: add vitest dependencies and config

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.js'],
+    globals: true
+  }
 })
